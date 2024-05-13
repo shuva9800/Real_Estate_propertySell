f6f87b72b714bb37cef8be78401b12032030d220
shuva9800 <ffshuva@gmail.com>
2024-05-13 23:48:02 +0530

    controller for update profile

--- backend/model/usermodel.js
+++ backend/model/usermodel.js
@@ -15,7 +15,11 @@ const userSchema = new mongoose.Schema({
     password:{
         type: "String",
         required: true,
-    }
+    },
+    // imageUrl:{
+    //     type:'string',
+    //     default:''
+    // }
 },
 {timestamps:true}
 );
