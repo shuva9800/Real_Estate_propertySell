f0d4613f4b36300e59116f88b46dbb22c82ff0c0
shuva9800 <ffshuva@gmail.com>
2024-07-31 23:39:09 +0530

    update and delete listing api controller and route

--- backend/routes/listing.route.js
+++ backend/routes/listing.route.js
@@ -1,12 +1,13 @@
 const express = require('express');
-const { createListing } = require('../controller/listing.controller');
+const { createListing, deleteListing, updateListing } = require('../controller/listing.controller');
 const { checkAuthentication } = require('../middleware/verifyUser');
 const router = express.Router();
 
 
 router.post('/create',checkAuthentication, createListing)
 
-
+router.delete('/delete/:id', checkAuthentication, deleteListing);
+router.post('/update/:id', checkAuthentication, updateListing);
 
 
 
